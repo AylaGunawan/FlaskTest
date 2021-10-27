f6e857c5a5b1c1891e0ca8104e7a92c3522b2f7b
AylaGunawan <ayla.gunawan@my.jcu.edu.au>
2021-10-27 17:46:25 +1000

    Initial commit; add decorators and functions to app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello World :)</h1>'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+
+@app.route('/f/<celsius>')
+def convert_celsius_to_fahrenheit(celsius):
+    """Convert celsius to fahrenheit."""
+    fahrenheit = float(celsius) * 9.0 / 5 + 32
+    return f"{celsius} C converts to {fahrenheit} F\n" \
+           f"<h2>Now that's useful :D</h2>"
+
+
+if __name__ == '__main__':
+    app.run()
